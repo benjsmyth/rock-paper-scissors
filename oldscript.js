067fc8f7babaf7a4bdf99dcc09594c4ac2564a9e
Ben Smyth <jbensmyth@gmail.com>
2018-05-26 19:03:37 -0400

    Add UI

---
+++ oldscript.js
@@ -0,0 +1,81 @@
+/* Corresponding values for Rock, Paper, and Scissors.
+
+Rock = 0;
+Paper = 1;
+Scissors = 2;
+
+*/
+
+// Beginning counters for player and computer.
+var playerCounter = 0;
+var computerCounter = 0;
+
+// Instructs the computer to choose one of three values at random.
+function computerPlay() {
+	return Math.floor(Math.random() * 3);
+}
+
+// Plays a single round of the game.
+function singleRound() {
+	var playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+	var computerSelection = computerPlay();
+
+	if (playerSelection === 'rock' && computerSelection === 0) {
+		alert('It\'s a tie!');
+	}	
+	else if (playerSelection === 'rock' && computerSelection === 1) {
+		alert('You lose! Paper beats rock.');
+		return computerCounter += 1;
+	}	
+	else if (playerSelection === 'rock' && computerSelection === 2) {
+		alert('You win! Rock beats scissors.');
+		return playerCounter += 1;
+	}	
+
+	if (playerSelection === 'paper' && computerSelection === 0) {
+		alert('You win! Paper beats rock.');
+		return playerCounter += 1;
+	}	
+	else if (playerSelection === 'paper' && computerSelection === 1) {
+		alert('It\'s a tie!');
+	}	
+	else if (playerSelection === 'paper' && computerSelection === 2) {
+		alert('You lose! Scissors beat paper.');
+		return computerCounter += 1;
+	}
+
+	if (playerSelection === 'scissors' && computerSelection === 0) {
+		alert('You lose! Rock beats scissors.');
+		return computerCounter += 1;
+	}	
+	else if (playerSelection === 'scissors' && computerSelection === 1) {
+		alert('You win! Scissors beat paper.');
+		return playerCounter += 1;
+	}
+	else if (playerSelection === 'scissors' && computerSelection === 2) {
+		alert('It\'s a tie!');
+	}
+}
+
+// Plays five rounds of the game, reports the score after each match, and then reports the final score and winner at the end.
+function newGame() {
+	singleRound();
+		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
+	singleRound();
+		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
+	singleRound();
+		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
+	singleRound();
+		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
+	singleRound();
+
+	if (playerCounter > computerCounter) {
+		alert(`Congratulations! You won with ${playerCounter} point(s). The computer finished with ${computerCounter}.`);
+	}	
+	else if (playerCounter < computerCounter) {
+		alert(`Rats! You lost with ${playerCounter} point(s). The computer won with ${computerCounter}.`);
+	}	
+	else {
+		alert(`Game end! It was a tie, with you and the computer both ending with ${playerCounter} point(s).`);
+	}
+}
--- script.js
+++ script.js
@@ -6,6 +6,71 @@ Scissors = 2;
 
 */
 
+window.onload = function() {
+	document.getElementById('music').play();
+	document.getElementById('music').volume = 0.35;
+}
+
+document.getElementById('rocksound').volume = 0.3;
+document.getElementById('papersound').volume = 0.3;
+document.getElementById('scissorssound').volume = 1;
+document.getElementById('winmusic').volume = 0.4;
+document.getElementById('losemusic').volume = 0.5;
+
+// Variables for DOM manipulation.
+var playerScore = document.querySelector('#playerscore');
+var computerScore = document.querySelector('#computerscore');
+var announceText = document.querySelector('#announcetext');
+
+// Functions for the event listeners.
+var playRock = function() {
+	playRound('rock');
+}
+
+var playPaper = function() {
+	playRound('paper');
+}
+
+var playScissors = function() {
+	playRound('scissors');
+}
+
+var rockSound = function() {
+	document.getElementById('rocksound').play();
+	document.getElementById('rocksound').currentTime = 0;
+}
+
+var paperSound = function() {
+	document.getElementById('papersound').play();
+	document.getElementById('papersound').currentTime = 0;
+}
+
+var scissorsSound = function() {
+	document.getElementById('scissorssound').play();
+	document.getElementById('scissorssound').currentTime = 0;
+}
+
+var winMusic = function() {
+	document.getElementById('winmusic').play();
+}
+
+var loseMusic = function() {
+	document.getElementById('losemusic').play();
+}
+
+// Event listeners for each icon.
+var rockimg = document.getElementById('rockimg');
+rockimg.addEventListener('click', playRock);
+rockimg.addEventListener('click', rockSound);
+
+var paperimg = document.getElementById('paperimg');
+paperimg.addEventListener('click', playPaper);
+paperimg.addEventListener('click', paperSound);
+
+var scissorsimg = document.querySelector('#scissorsimg');
+scissorsimg.addEventListener('click', playScissors);
+scissorsimg.addEventListener('click', scissorsSound);
+
 // Beginning counters for player and computer.
 var playerCounter = 0;
 var computerCounter = 0;
@@ -16,70 +81,123 @@ function computerPlay() {
 }
 
 // Plays a single round of the game.
-function singleRound() {
-	var playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+function playRound(selection) {
+	var playerSelection = selection;
 	var computerSelection = computerPlay();
 
 	if (playerSelection === 'rock' && computerSelection === 0) {
-		alert('It\'s a tie!');
-
-	}	else if (playerSelection === 'rock' && computerSelection === 1) {
-		alert('You lose! Paper beats rock.');
-		return computerCounter += 1;
-
-	}	else if (playerSelection === 'rock' && computerSelection === 2) {
-		alert('You win! Rock beats scissors.')
-		return playerCounter += 1;
-
+		playerCounter += 0;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `It\'s a tie! The score is ${playerCounter} to ${computerCounter}.`
+	}	
+	else if (playerSelection === 'rock' && computerSelection === 1) {
+		playerCounter += 0;
+		computerCounter += 1;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You lose! Paper beats rock. The score is ${playerCounter} to ${computerCounter}.`
+	}	
+	else if (playerSelection === 'rock' && computerSelection === 2) {
+		playerCounter += 1;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You win! Rock beats scissors. The score is ${playerCounter} to ${computerCounter}.`		
 	}	
 
 	if (playerSelection === 'paper' && computerSelection === 0) {
-		alert('You win! Paper beats rock.');
-		return playerCounter += 1;
-
-	}	else if (playerSelection === 'paper' && computerSelection === 1) {
-		alert('It\'s a tie!');
-
-	}	else if (playerSelection === 'paper' && computerSelection === 2) {
-		alert('You lose! Scissors beat paper.')
-		return computerCounter += 1;
-
+		playerCounter += 1;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You win! Paper beats rock. The score is ${playerCounter} to ${computerCounter}.`	
+	}	
+	else if (playerSelection === 'paper' && computerSelection === 1) {
+		playerCounter += 0;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `It\'s a tie! The score is ${playerCounter} to ${computerCounter}.`
+	}	
+	else if (playerSelection === 'paper' && computerSelection === 2) {
+		playerCounter += 0;
+		computerCounter += 1;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You lose! Scissors beat paper. The score is ${playerCounter} to ${computerCounter}.`
 	}
 
 	if (playerSelection === 'scissors' && computerSelection === 0) {
-		alert('You lose! Rock beats scissors.');
-		return computerCounter += 1;
-
-	}	else if (playerSelection === 'scissors' && computerSelection === 1) {
-		alert('You win! Scissors beat paper.');
-		return playerCounter += 1;
-
-	}	else if (playerSelection === 'scissors' && computerSelection === 2) {
-		alert('It\'s a tie!')
-
+		playerCounter += 0;
+		computerCounter += 1;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You lose! Rock beats scissors. The score is ${playerCounter} to ${computerCounter}.`
+	}	
+	else if (playerSelection === 'scissors' && computerSelection === 1) {
+		playerCounter += 1;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `You win! Scissors beat paper. The score is ${playerCounter} to ${computerCounter}.`	
 	}
-}
+	else if (playerSelection === 'scissors' && computerSelection === 2) {
+		playerCounter += 0;
+		computerCounter += 0;
+		playerScore.textContent = String(playerCounter);
+		computerScore.textContent = String(computerCounter);
+		announceText.textContent = `It\'s a tie! The score is ${playerCounter} to ${computerCounter}.`
+	}
+	if (playerCounter === 5 && playerCounter > computerCounter) {
+		announceText.textContent = `Congratulations! You won against the computer. Refresh the browser if you'd like to play again.`
+		rockimg.removeEventListener('click', playRock);
+		paperimg.removeEventListener('click', playPaper);
+		scissorsimg.removeEventListener('click', playScissors);
 
-// Plays five rounds of the game, reports the score after each match, and then reports the final score and winner at the end.
-function newGame() {
-	singleRound();
-		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
-	singleRound();
-		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
-	singleRound();
-		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
-	singleRound();
-		alert(`Player has ${playerCounter} ||| Computer has ${computerCounter}`);
-	singleRound();
+		rockimg.removeEventListener('click', rockSound);
+		paperimg.removeEventListener('click', paperSound);
+		scissorsimg.removeEventListener('click', scissorsSound);
 
-	if (playerCounter > computerCounter) {
-		alert(`Congratulations! You won with ${playerCounter} point(s). The computer finished with ${computerCounter}.`);
+		rockimg.classList.replace('startgame', 'endgame');
+		paperimg.classList.replace('startgame', 'endgame');
+		scissorsimg.classList.replace('startgame', 'endgame');
 
-	}	else if (playerCounter < computerCounter) {
-		alert(`Rats! You lost with ${playerCounter} point(s). The computer won with ${computerCounter}.`);
+		music.pause();
+		winMusic();
+	}	
+	else if (computerCounter === 5 && playerCounter < computerCounter) {
+		announceText.textContent = `Rats! The computer beat you. Refresh the browser if you'd like to play again.`;
+		rockimg.removeEventListener('click', playRock);
+		paperimg.removeEventListener('click', playPaper);
+		scissorsimg.removeEventListener('click', playScissors);
 
-	}	else {
-		alert(`Game end! It was a tie, with you and the computer both ending with ${playerCounter} point(s).`);
+		rockimg.removeEventListener('click', rockSound);
+		paperimg.removeEventListener('click', paperSound);
+		scissorsimg.removeEventListener('click', scissorsSound);
 
+		rockimg.classList.replace('startgame', 'endgame');
+		paperimg.classList.replace('startgame', 'endgame');
+		scissorsimg.classList.replace('startgame', 'endgame');
+
+		music.pause();
+		loseMusic();
+	}	
+	else if (playerCounter === 5 && computerCounter === 5) {
+		announceText.textContent = `Game end! It was a tie. Refresh the browser if you'd like to play again.`;
+		rockimg.removeEventListener('click', playRock);
+		paperimg.removeEventListener('click', playPaper);
+		scissorsimg.removeEventListener('click', playScissors);
+
+		rockimg.removeEventListener('click', rockSound);
+		paperimg.removeEventListener('click', paperSound);
+		scissorsimg.removeEventListener('click', scissorsSound);
+
+		rockimg.classList.replace('startgame', 'endgame');
+		paperimg.classList.replace('startgame', 'endgame');
+		scissorsimg.classList.replace('startgame', 'endgame');
+
+		music.pause();
 	}
 }
